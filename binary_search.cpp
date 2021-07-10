db2df725cdaf7826c81ac6a5c7bcac26fcfd1e94
Arnab10 <arnabhattacharya10@gmail.com>
2021-07-10 14:02:34 +0530

    commit ok

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-#include <iostream>
 
+#include <iostream>
 using namespace std;
 
 int binarySearch(int *p,int low, int high,int key);
@@ -24,30 +23,3 @@ int binarySearch(int *p,int low, int high,int key){
 	else 
 		return binarySearch(p,mid+1,high,key);
 }
-=======
-#include <iostream>
-
-using namespace std;
-
-int binarySearch(int *p,int low, int high,int key);
-int main()
-{
-	int arr[] = {2,3,4,5,6,7,8,9};
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int key;
-	cout<<"Enter the key to find";
-	cin>>key;
-	int index = binarySearch(arr,0,n-1,key);
-	cout<<"Value found at index->"<<index;
-	return 0;
-}
-int binarySearch(int *p,int low, int high,int key){
-	int mid = low + (high-low)/2;
-	if(p[mid]==key) 
-		return mid;
-	else if (p[mid]>key)
-		return binarySearch(p,low,mid,key);
-	else 
-		return binarySearch(p,mid+1,high,key);
-}
->>>>>>> 9429c732f425f18586d4435ef35fe00c8c284123
