ea47c49b8f54b468de2c7eeee154489236a08d93
Arnab10 <arnabhattacharya10@gmail.com>
2021-07-15 23:39:49 +0530

    changes

--- A2OJ-Codeforces/SuperCentralpoint.cpp
+++ A2OJ-Codeforces/SuperCentralpoint.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 //using namespace std;
 #define degree(x) (x)*180/M_PI
-#define isTop(x,y) (imag(x) > imag(y)) && (real(x) == real(y))
-#define isRight(x,y) (imag(x) == imag(y)) && (real(x)>real(y))
-#define isLeft(x,y) (imag(x) == imag(y)) && (real(x)<real(y))
-#define isButtom(x,y) (imag(x) < imag(y)) && (real(x) == real(y))
+#define isTop(x,y) (x.second > y.second) && (x.first == y.first)
+#define isRight(x,y) (x.second == y.second) && (x.first>y.first)
+#define isLeft(x,y) (x.second == y.second) && (x.first<y.first)
+#define isButtom(x,y) (x.second < y.second) && (x.first == y.first)
 
 using namespace std;
 
@@ -14,14 +14,14 @@ int main()
     int x,y;
     int left ,right , top , buttom;
     int f = 100;
-    complex <int> points;
-    complex <int> reference;
-    vector <complex<int>> plane;
+    pair <int,int> points;
+    pair <int,int> reference;
+    vector <pair<int,int>> plane;
     cin>>n;
     for(int i=0;i<n;i++){
         cin>>x;
         cin>>y;
-        points = {x,y};
+        points = make_pair(x,y);
         plane.push_back(points);
     }   
     for(int i=0;i<n;i++){
