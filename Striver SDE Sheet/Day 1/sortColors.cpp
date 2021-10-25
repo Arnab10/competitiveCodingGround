eb1886ea6cabfa6f9122e9da9375675c1b57f366
Arnab10 <arnabhattacharya10@gmail.com>
2021-10-25 23:46:11 +0530

    Day 1 Problem 5

--- Striver SDE Sheet/Day 1/sortColors.cpp
+++ Striver SDE Sheet/Day 1/sortColors.cpp
@@ -4,14 +4,13 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-    map<int,int> hist;
-    int t=0,c=0;
-    for(int i=0;i<nums.size();i++) hist[nums[i]]++;
-    for(auto j : hist){
-        t = j.second;
-        while(t--)
-            nums[c++] = j.first;
-    }
-    //display(nums);
+        map<int,int> hist;
+        int t=0,c=0;
+        for(int i=0;i<nums.size();i++) hist[nums[i]]++;
+        for(auto j : hist){
+            t = j.second;
+            while(t--)
+                nums[c++] = j.first;
+        }
     }
 };
