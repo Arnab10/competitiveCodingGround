cb527a496515d2376bbcf3f05adc3bb94e430cd7
Arnab10 <arnabhattacharya10@gmail.com>
2021-10-30 11:43:24 +0530

    my Changes for Day 2 Problem 2

--- Striver SDE Sheet/Day 2/merge-intervals.cpp
+++ Striver SDE Sheet/Day 2/merge-intervals.cpp
@@ -15,24 +15,21 @@ void display(vector<vector<int>>&matrix){
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> mergedIntervals;
-        if(intervals.size() == 0) {
-            return mergedIntervals; 
-        }
-        sort(intervals.begin(), intervals.end());
-        vector<int> tempInterval = intervals[0]; 
-        
-        for(auto it : intervals) {
-            if(it[0] <= tempInterval[1]) {
-                tempInterval[1] = max(it[1], tempInterval[1]); 
-            } else {
-                mergedIntervals.push_back(tempInterval); 
-                tempInterval = it; 
+        vector<int> temp;
+        vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        temp = intervals[0];
+        for(auto it : intervals){
+            if(it[0]<=temp[1]){
+                temp[1] = max(it[1],temp[1]);
+            }
+            else{
+                ans.push_back(temp);
+                temp = it;
             }
         }
-        mergedIntervals.push_back(tempInterval);
-        display(mergedIntervals);
-        return mergedIntervals; 
+        ans.push_back(temp);
+        return ans;
     }
 };
 
