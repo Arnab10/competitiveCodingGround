73c1ab121880bb34c81d1855c7078eb33a3e4cc9
Arnab10 <arnabhattacharya10@gmail.com>
2021-11-05 20:53:54 +0530

    Day 4 Problem 4

---
+++ Striver SDE Sheet/Day 4/largestSubArrayWith0.cpp
@@ -0,0 +1,34 @@
+//https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1#
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int maxLen(vector<int>&nums, int n)
+    {   
+        // Your code here
+        int sum=10,len=0;
+        unordered_map<int,int> maps;
+        maps[10] = -1;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            ///cout<<sum<<"\t";
+            if(maps.find(sum)!=maps.end()){
+                len = max(len,i-maps[sum]);
+                //cout<<"holo naki for i = "<<i<<'\n';
+                continue;
+            }
+            maps[sum] = i;
+            
+        }
+        return len;
+    }
+};
+
+int main(){
+    vector<int> vec = {-1, 1, -1, 1};
+    Solution sol;
+    cout<<sol.maxLen(vec,4);
+    return 0;
+}
