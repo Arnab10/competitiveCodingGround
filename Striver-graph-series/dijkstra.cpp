f919b7b5ef443e5186a3d1a513993dda3cf9d2fd
Arnab10 <arnabhattacharya10@gmail.com>
2022-07-10 08:48:54 +0530

    commit dijkstra

--- Striver-graph-series/dijkstra.cpp
+++ Striver-graph-series/dijkstra.cpp
@@ -19,24 +19,26 @@ int main(){
 
 	// Dijkstra's algorithm begins from here
 	priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-    vector<int> distTo(n+1,INT_MAX);
-    distTo[source]=0;
-    pq.push(make_pair(0,source)); // (dist,source)
-    while(!pq.empty()){
-        int dist = pq.top().first;
-        int prev = pq.top().second;
-        pq.pop();
-        
-        for(auto it : g[prev]){
-            int next = it.first;
-            int nextDist = it.second;
+	vector<int> distTo(n+1,INT_MAX);
+	distTo[source]=0;
+	pq.push({0,source});
 
-            if(distTo[next]>dist+nextDist){
-                distTo[next] = dist+nextDist;
-                pq.push(make_pair(distTo[next],next));
-            }
-        }
-    }
+	while(!pq.empty()){
+		int dist = pq.top().first;
+		int prev = pq.top().second;
+		pq.pop();
+
+		for (auto it: g[prev]){
+			int next = it.first;
+			int nextDist = it.second;
+
+			if(distTo[next]>dist + nextDist){
+				distTo[next] = dist + nextDist;
+				pq.push({distTo[next],next});
+			}
+		}
+
+	}
 
 	cout << "The distances from source " << source << " are : \n";
 	for(int i = 1 ; i<=n ; i++)	cout << distTo[i] << " ";
